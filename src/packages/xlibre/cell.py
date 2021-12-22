0870f58300e84a1b17d8135200600c617ae02a21
Henrique Moreira <h@serrasqueiro.com>
2021-12-22 21:26:08 +0000

    First draft of xlibre

---
+++ src/packages/xlibre/cell.py
@@ -0,0 +1,52 @@
+# (c)2021  Henrique Moreira
+
+""" cell -- OpenLibre cell wrapper
+"""
+
+# pylint: disable=missing-function-docstring
+
+import datetime
+
+
+class Textual():
+    """ Textual cell """
+    def __init__(self, cell):
+        self._cell = cell
+        self.value = None
+        self.string = ""
+        if cell is not None:
+            self.string = self._from_cell(cell)
+
+    def empty(self) -> bool:
+        return self._cell is None
+
+    def __str__(self) -> str:
+        return self.string
+
+    def __repr__(self):
+        if isinstance(self.value, (int, float)):
+            return self.string
+        return "'" + self.string + "'"
+
+    def _from_cell(self, cell):
+        value = cell.value
+        datatype = cell.data_type
+        if datatype == "d":	# date/ date and time
+            if isinstance(value, datetime.time):
+                t_format = "%H:%M:%S"
+            else:
+                t_format = "%Y-%d-%m"
+            try:
+                text = value.strftime(t_format)
+            except AttributeError:
+                text = "?"
+        elif datatype == "n":	# numeric
+            text = str(value)
+        else:
+            text = value
+        self.value = value
+        return text
+
+
+if __name__ == "__main__":
+    print("Import me!")
---
+++ src/packages/xlibre/libre.py
@@ -0,0 +1,158 @@
+# (c)2021  Henrique Moreira
+
+""" libre -- OpenLibre wrapper, wrapping openpyxl for Excel documents
+"""
+
+# pylint: disable=missing-function-docstring
+
+import openpyxl
+from xlibre.cell import Textual
+
+
+class GeneralTabular():
+    """ General Tabular abstract class """
+    def _simple_column(self, alist:list) -> str:
+        def better_str(astr:str):
+            return astr.replace("\t", " ").replace(" ", "_").strip()
+
+        res = '+'.join([better_str(item.string) for item in alist])
+        return res
+
+
+class Heading(GeneralTabular):
+    """ Sheet heading """
+    def __init__(self, sht=None):
+        self._sheet = sht
+        self._headings = {
+            "@lines": [],
+            "@columns": [],
+            "by-column": {},
+            "by-name": {},
+            "by-column-letter": {},
+        }
+
+    def columns(self) -> list:
+        return self._headings["@columns"]
+
+    def by_column(self) -> dict:
+        return self._headings["by-column"]
+
+    def get_columns(self, by_what:str) -> str:
+        return self._headings[by_what]
+
+    def _my_hash(self, result:dict):
+        cols = result["@columns"]
+        for col_idx, alist in cols:
+            result["by-column"][col_idx] = self._simple_column(alist)
+        for col_idx, alist in cols:
+            name = result["by-column"][col_idx]
+            result["by-name"][name] = col_idx
+
+    def parse(self, start_row:int, end_row:int):
+        first_col = -1
+        fields, cols = [], {}
+        y_row = start_row
+        if self._headings["@lines"]:
+            return False
+        while y_row <= end_row:
+            x_col = 1
+            while x_col < 10 ** 3:	# max 1000 columns
+                text = self._sheet.cell(row=y_row, column=x_col)
+                if text.value is None:
+                    first_col = x_col
+                    break
+                field = Textual(text)
+                fields.append(field)
+                if x_col not in cols:
+                    cols[x_col] = [field]
+                else:
+                    cols[x_col].append(field)
+                x_col += 1
+            self._headings["@lines"].append((y_row, fields))
+            fields = []
+            y_row += 1
+        for x_col in sorted(cols):
+            self._headings["@columns"].append((x_col, cols[x_col]))
+        self._my_hash(self._headings)
+        return True
+
+
+class Content():
+    """ Sheet content """
+    def __init__(self, sht=None, desc="", start_content_row=1):
+        self._sheet = sht
+        self._description = desc
+        self._start_row = int(start_content_row)
+        self.rows = []
+
+    def start_row(self) -> int:
+        return self._start_row
+
+    def parse(self) -> bool:
+        if self._sheet is None:
+            return False
+        idx = 0
+        for row in self._sheet.rows:
+            idx += 1
+            if idx < self._start_row:
+                continue
+            alist = [Textual(item) for item in row]
+            self.rows.append(alist)
+        return True
+
+class Book():
+    """ Workbook class """
+    def __init__(self, path=""):
+        self._path, self._wbk = path, None
+        if path:
+            self._load_book()
+        self.headings = [Heading()]
+        self.contents = [Content(desc=path)]
+
+    def workbook(self):
+        return self._wbk
+
+    def sheet_names(self) -> list:
+        """ Returns the list of sheet names """
+        if not self._wbk:
+            return []
+        return self._wbk.sheetnames
+
+    def sheet_by_index(self, idx1:int):
+        """ Returns the sheet indicated by the index, 'idx1', ranging from 1 to n (sheets).
+        """
+        if idx1 < 1:
+            return None
+        if not self._wbk or idx1 > len(self._wbk.sheetnames):
+            return None
+        name = self._wbk.sheetnames[idx1 - 1]
+        sht = self._wbk[name]
+        return sht
+
+    def parse_headings(self, start_row:int=1, end_row:int=1) -> bool:
+        if not self._wbk:
+            return False
+        if len(self.headings) > 1:
+            self.headings = self.headings[:1]
+            self.contents = self.contents[:1]
+        for sheetname in self._wbk.sheetnames:
+            sht = self._wbk[sheetname]
+            head = Heading(sht)
+            head.parse(start_row, end_row)
+            self.headings.append(head)
+            self.contents.append(Content(sht, sheetname, end_row+1))
+        return True
+
+    def parse(self, idx1:int) -> bool:
+        """ Parse sheet with index 'idx1'
+        """
+        if idx1 < 0:
+            return False
+        return self.contents[idx1].parse()
+
+    def _load_book(self) -> str:
+        self._wbk = openpyxl.load_workbook(self._path, read_only=True)
+
+
+if __name__ == "__main__":
+    print("Import me!")
